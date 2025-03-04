34e8f46292875e48329ca944c6bc923099ffa57f
vikriyuwi <latifulfikri@outlook.com>
2025-03-04 07:44:37 +0700

    Initial Commit

---
+++ learningUIKit/FirstScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learningUIKit
+//
+//  Created by Vikri Yuwi on 03/03/25.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
